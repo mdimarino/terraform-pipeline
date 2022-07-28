731641d5c0fe0459cf521eea4cbc7a24387348ac
Marcello Di Marino <marcello@di.marino.nom.br>
2022-07-28 11:04:26 -0300

    Dados da VPC para criar o EKS

--- iac/servico1-eks/main.tf
+++ iac/servico1-eks/main.tf
@@ -37,14 +37,14 @@ module "eks" {
   service                    = local.service
   environment                = local.environment
   kubernetes_version         = "1.22"
-  vpc_id                     = "vpc-03ed1ad5c50ebc8de"
+  vpc_id                     = "vpc-0f217e14ca4ec73f5"
   endpoint_private_access    = true
   endpoint_public_access     = true
   public_access_cidrs        = ["0.0.0.0/0"]
   enabled_cluster_log_types  = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
   service_ipv4_cidr          = "192.168.0.0/16"
-  public_subnets_ids         = ["subnet-08b3d79d7e9cb5769", "subnet-0ec5480126fab209c", "subnet-070341abe45a40162"]
-  private_subnets_ids        = ["subnet-0e410448dd031cf89", "subnet-01247b9cc4c06974e", "subnet-0efa14b4682dcb0cb"]
+  public_subnets_ids         = ["subnet-0c26a41cfc091b700", "subnet-03d5a12462dd75543", "subnet-090a031027cc88aaa"]
+  private_subnets_ids        = ["subnet-011054cc43dad6582", "subnet-0e1417416e150dbdb", "subnet-024dececd3160a535"]
   fargate_profile_namespaces = ["default", "kube-system", "${local.service}-${local.environment}"]
   eks_node_groups            = []
 }
