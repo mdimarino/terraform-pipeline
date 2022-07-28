ff380609b400fe37654c5704003250554cfb580e
Marcello Di Marino <marcello@di.marino.nom.br>
2022-07-28 10:52:05 -0300

    output com nome do módulo correto

--- iac/servico1-vpc/output.tf
+++ iac/servico1-vpc/output.tf
@@ -1,24 +1,24 @@
 output "vpc_id" {
   description = "O ID da VPC"
-  value       = module.network.vpc_id
+  value       = module.vpc.vpc_id
 }
 
 output "public_subnets_ids" {
   description = "Os IDs das sub redes públicas"
-  value       = module.network.public_subnets_ids
+  value       = module.vpc.public_subnets_ids
 }
 
 output "private_subnets_ids" {
   description = "Os IDs das sub redes privadas"
-  value       = module.network.private_subnets_ids
+  value       = module.vpc.private_subnets_ids
 }
 
 output "database_subnets_ids" {
   description = "Os IDs das sub redes de base de dados"
-  value       = module.network.database_subnets_ids
+  value       = module.vpc.database_subnets_ids
 }
 
 output "ips_externos" {
   description = "Os IPs externos usados nos NAT Gateways"
-  value       = module.network.nat_gateways_ips
+  value       = module.vpc.nat_gateways_ips
 }
